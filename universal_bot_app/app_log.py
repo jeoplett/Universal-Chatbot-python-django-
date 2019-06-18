7dd35ac00514b6133126c50911a8c489b82e65e5
Svyatoslav Lunev <jeoplett@gmail.com>
2019-06-18 23:28:15 +0300

    transition to python 3

--- universal_bot_app/app_log.py
+++ universal_bot_app/app_log.py
@@ -3,4 +3,4 @@
 import datetime
 
 def appendLog(log_string):
-    print "[" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '] ' + log_string
+    print '[' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '] ' + log_string
